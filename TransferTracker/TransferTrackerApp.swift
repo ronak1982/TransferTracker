76b61c58fe9befc98ffb27ce88cc497bb738f6ba
ronak1982 <ronak007@gmail.com>
2026-01-07 15:41:50 -0500

    Initial Commit

---
+++ TransferTracker/TransferTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TransferTrackerApp.swift
+//  TransferTracker
+//
+//  Created by Ronak Patel on 1/7/26.
+//
+
+import SwiftUI
+
+@main
+struct TransferTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
